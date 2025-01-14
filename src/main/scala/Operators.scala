34d8c5861a02e0ceaca9c64abe1509ba5799fb22
mayank <mayankrao687@gmail.com>
2025-01-14 20:36:11 +0530

    My first Program

---
+++ src/main/scala/Operators.scala
@@ -0,0 +1,16 @@
+object Operators {
+  def main(args: Array[String]): Unit = {
+    var Age= 15
+    if (Age < 13)
+    {
+      println("Child")
+    }
+    else if (Age >= 13 && Age < 20)
+    {
+      println("Teenager")
+    } else
+    {
+      println("Adult")
+    }
+  }
+}
